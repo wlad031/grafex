3be00f1d77f839256c8085610b1b716df099590d
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2020-12-17 15:22:18 +0100

    #5 Add scaladocs and unit test for configuration loading

--- src/main/scala/com/grafex/core/boot/Config.scala
+++ src/main/scala/com/grafex/core/boot/Config.scala
@@ -1,48 +1,83 @@
-package com.grafex.core.boot
+package com.grafex
+package core
+package boot
 
 import cats.data.EitherT
 import cats.effect.IO
 import cats.implicits.toBifunctorOps
-import com.grafex.core.GrafexError
-import com.typesafe.config.{ConfigFactory, ConfigParseOptions, Config => TConfig}
+import com.typesafe.config.{ ConfigFactory, ConfigParseOptions, Config => TypesafeConfig }
 import io.circe.Json
 import pureconfig.ConfigReader.Result
 import pureconfig.backend.ErrorUtil.unsafeToReaderResult
 import pureconfig.error.CannotReadFile
 import pureconfig.generic.auto._
 import pureconfig.module.circe._
-import pureconfig.{ ConfigObjectSource, ConfigReader, ConfigSource }
+import pureconfig.{ ConfigObjectSource, ConfigSource }
 
 import java.nio.file.Path
 
+/** Contains all the functionality for reading application configuration files.
+  *
+  * @example {{{
+  * val startupContext: Startup.Context = ??? // some startup context
+  *
+  * import com.grafex.core.boot.Config
+  * import com.grafex.core.boot.Config.{ ConfigurationReadingError, GrafexConfiguration }
+  *
+  * val config: EitherT[IO, ConfigurationReadingError, GrafexConfiguration] = Config.read(startupContext)()
+  * }}}
+  */
 object Config {
 
-  def read(
+  /** Loads the [[GrafexConfiguration]].
+    *
+    * Sources (descending precedence):
+    *   1. config files specified as argument parameters;
+    *   2. default config;
+    *   3. "baked" in the application resources.
+    *
+    * @param context the startup context
+    * @param defaultConfigPath default config path (default value
+    *                          is built using [[Config.buildHomeConfigPath(Startup.Context)]])
+    * @return [[EitherT]] of [[IO]] with loaded config or [[ConfigurationReadingError]]
+    */
+  def load(
     context: Startup.Context
   )(
-    defaultConfig: Path = homeConfig(context)
-  ): EitherT[IO, ConfigReadingError, GrafexConfig] = {
+    defaultConfigPath: Path = buildHomeConfigPath(context)
+  ): EitherT[IO, ConfigurationReadingError, GrafexConfiguration] = {
 
     val sources = context.configPaths.reverse
       .map(ConfigSource.file)
       .foldLeft(ConfigSource.empty)(_ withFallback _)
-      .withFallback(optionalFileSource(defaultConfig))
+      .withFallback(optionalFileSource(defaultConfigPath))
       .withFallback(ConfigSource.default)
 
     val load = IO {
-      sources.load[GrafexConfig].leftMap(f => ConfigReadingError(f.prettyPrint(2)))
+      sources.load[GrafexConfiguration].leftMap(f => ConfigurationReadingError(f.prettyPrint(2)))
     }
 
     EitherT(load)
   }
 
-  case class GrafexConfig(
+  /** Returns the default config path which will be located in `$HOME/.config/grafex/grafex.conf`.
+    *
+    * @param context the startup context
+    */
+  def buildHomeConfigPath(context: Startup.Context): Path = {
+    val dirPath = context.userHome.resolve(".config/grafex")
+    dirPath.resolve("grafex.conf")
+  }
+
+  /** Contains all the application configurations. */
+  case class GrafexConfiguration(
     accountId: String,
-    metaDataSource: GrafexConfig.MetaDataSourceConfig,
+    metaDataSource: GrafexConfiguration.MetaDataSourceConfig,
     modes: List[Json]
   )
 
-  object GrafexConfig {
+  /** Contains inner structures of the [[GrafexConfiguration]] */
+  object GrafexConfiguration {
 
     sealed trait MetaDataSourceConfig
 
@@ -51,21 +86,20 @@ object Config {
     ) extends MetaDataSourceConfig
   }
 
-  case class ConfigReadingError(msg: String) extends GrafexError
+  /** Represents an error of config reading. */
+  case class ConfigurationReadingError(msg: String) extends GrafexError
 
+  /** `pureconfig` settings which allow to not fail on non-existing configuration files. */
   private val notStrictSettings: ConfigParseOptions = ConfigParseOptions.defaults.setAllowMissing(true)
 
-  private def parseOptionalFile(path: Path): Result[TConfig] = {
+  /** Parses the config file which can be missing. */
+  private def parseOptionalFile(path: Path): Result[TypesafeConfig] = {
     unsafeToReaderResult(
       ConfigFactory.parseFile(path.toFile, notStrictSettings),
       onIOFailure = Some(CannotReadFile(path, _))
     )
   }
 
+  /** Returns [[ConfigObjectSource]] for file which can be missing. */
   private def optionalFileSource(path: Path): ConfigObjectSource = ConfigObjectSource(parseOptionalFile(path))
-
-  def homeConfig(context: Startup.Context): Path = {
-    val dirPath = context.userHome.resolve(".config/grafex")
-    dirPath.resolve("grafex.conf")
-  }
 }
